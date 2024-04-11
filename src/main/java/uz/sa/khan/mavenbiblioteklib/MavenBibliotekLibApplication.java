a31c2cf5d3376072d8673f3ce72fd85d06210379
UzmaSabir <uzmasdhan@yahoo.com>
2024-04-11 14:18:12 +0200

    add basic commit

---
+++ src/main/java/uz/sa/khan/mavenbiblioteklib/MavenBibliotekLibApplication.java
@@ -0,0 +1,13 @@
+package uz.sa.khan.mavenbiblioteklib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenBibliotekLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenBibliotekLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/sa/khan/mavenbiblioteklib/MavenBibliotekLibApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.sa.khan.mavenbiblioteklib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenBibliotekLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
